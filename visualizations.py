1c377be307a3bcf3ca9a574c07034d347c4bc7be
sundar <y2kprabu@gmail.com>
2026-03-22 20:46:00 +0000

    refactor: adjust pie chart text position for improved layout

--- visualizations.py
+++ visualizations.py
@@ -56,7 +56,7 @@ def build_pie_figure(section_data, selected_section):
         "legend": {"type": "scroll", "orient": "vertical", "right": "100px", "bottom": "10px"},
         "graphic": [{
             "type": "text",
-            "left": "30%",
+            "left": "40%",
             "top": "middle",
             "style": {
                 "text": f"{selected_meta['name']}\n{int(selected_meta['completed'])} of {int(selected_meta['total'])} done",
